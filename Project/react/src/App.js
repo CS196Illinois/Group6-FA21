e63008ef51ef6f6fa5cabe4968f26428bcd579ee
akrakman <akrakman@gmail.com>
2021-11-03 20:25:51 -0500

    Revert "Revert "Revert "buttons"""
    This reverts commit 6e6a481a0376ef87ad315a5e80e620ce1c852054.

--- Project/react/src/App.js
+++ Project/react/src/App.js
@@ -8,33 +8,27 @@ function App() {
     setData(val.target.value)
     console.warn(val.target.value)
   }
-
   const USD = true;
 
   return (
     <div className="App">
       <h1>Currency Converter</h1>
-      <br></br>
-      <h3>As of (present time), 1 {USD ? 'United States Dollar' : 'Euro'} equals</h3>
-      <h1> 0.86 Euro</h1>
-      <input type = "number" onChange={getData} placeholder="0" 
-      step="0.5" min="0" max="100" onChange={getData}></input>
+      <h2>1 {USD ? 'United States Dollar' : 'Euro'} equals</h2>
+      <h1> {data} Euro</h1>
+      <input type = "number" onChange={getData}></input>
       <select>
         <option value = "USD">USD</option>
         <option value = "EUR">EUR</option>
       </select>
       <br></br>
       <br></br>
-      <input type = "number" placeholder="0" step="0.5" min="0" max="100" onChange={getData}></input>
+      <input type = "number" onChange={getData}></input>
       <select>
         <option value = "USD">USD</option>
         <option value = "EUR">EUR</option>
         </select>
       <br></br>
       <br></br>
-        <GroupButtons />
-      <br></br>
-      <h3>  (dollars/euros) equals 29 (opposite amount)</h3>
     </div>
   );
 }
